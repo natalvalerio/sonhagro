1558bb410793fa0bccb4f788675dbea03e815844
natalvalerio <natalvalerio@gmail.com>
2023-11-29 15:37:29 -0300

    *

--- up.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-echo ---------------------------------------------
-echo %1
-echo ---------------------------------------------
-git add "%1"
-git commit -m "%1"
-git push -u origin main
-rem git status
