113d4c35fefbe19865ce67ed54208c6300b57d01
natalvalerio <natalvalerio@gmail.com>
2024-05-22 12:54:51 -0300

    site

--- seguro/js/js.js
+++ seguro/js/js.js
@@ -1,58 +1,66 @@
 const ramosSeguros = [
-    'Automóvel 🚘',
-    'Caminhão 🚛',
-    'Motocicleta 🏍️',
-    'Empresarial 🏭',
-    'Residencial 🏠',
-    'Vida ❤️',
-    'Condomínio 🏢',
-    'Rural 🚜',
-    'Outros 💼'
+    'AUTOMÓVEL   🚘',
+    'CAMINHÃO    🚛',
+    'MOTOCICLETA 🏍️',
+    'EMPRESARIAL 🏭',
+    'RESIDENCIAL 🏠',
+    'VIDA        ❤️',
+    'CONDOMÍNIO  🏢',
+    'RURAL       🚜',
+    'OUTROS      💼'
 ];
 
 const seguradoras = [
-    { seguradora: 'AIG', ramos: ['', '1', '', '1', '', '', '', '1', '1'], link: 'https://www.aig.com.br' },
-    { seguradora: 'AKAD', ramos: ['', '', '', '', '', '', '', '', '1'], link: 'https://www.akad.com.br' },
-    { seguradora: 'ALLIANZ', ramos: ['1', '1', '1', '1', '1', '1', '1', '1', '1'], link: 'https://www.allianz.com.br' },
-    { seguradora: 'ARGO', ramos: ['', '', '', '', '', '', '', '', '1'], link: 'https://www.argo.com.br' },
-    { seguradora: 'AXA', ramos: ['', '', '', '', '', '', '', '', '1'], link: 'https://www.axa.com.br' },
-    { seguradora: 'BERKLEY', ramos: ['', '', '', '', '', '', '', '', '1'], link: 'https://www.berkley.com.br' },
-    { seguradora: 'BRADESCO', ramos: ['1', '1', '1', '1', '1', '1', '1', '', ''], link: 'https://www.bradescoseguros.com.br' },
-    { seguradora: 'CHUBB', ramos: ['', '', '', '', '', '', '', '', '1'], link: 'https://www.chubb.com.br' },
-    { seguradora: 'ESSOR', ramos: ['', '', '', '', '', '', '', '', '1'], link: 'https://www.essor.com.br' },
-    { seguradora: 'EXCELSIOR', ramos: ['', '', '', '', '', '', '', '', '1'], link: 'https://www.excelsior.com.br' },
-    { seguradora: 'EZZE', ramos: ['', '', '', '', '', '', '', '', '1'], link: 'https://www.ezze.com.br' },
-    { seguradora: 'FAIRFAX', ramos: ['', '', '', '', '', '', '', '', '1'], link: 'https://www.fairfax.com.br' },
-    { seguradora: 'FATOR', ramos: ['', '', '', '', '', '', '', '', '1'], link: 'https://www.fator.com.br' },
-    { seguradora: 'HDI', ramos: ['1', '1', '1', '1', '1', '', '1', '', ''], link: 'https://www.hdi.com.br' },
-    { seguradora: 'KOVR', ramos: ['', '', '', '', '', '', '', '', '1'], link: 'https://www.kovr.com.br' },
-    { seguradora: 'LIBERTY', ramos: ['1', '1', '', '1', '1', '1', '', '', ''], link: 'https://www.liberty.com.br' },
-    { seguradora: 'MAG', ramos: ['', '', '', '', '', '1', '', '', ''], link: 'https://www.mag.com.br' },
-    { seguradora: 'MAPFRE', ramos: ['1', '1', '1', '1', '1', '1', '', '1', ''], link: 'https://www.mapfre.com.br' },
-    { seguradora: 'METLIFE', ramos: ['', '', '', '', '', '1', '', '', ''], link: 'https://www.metlife.com.br' },
-    { seguradora: 'NEWE', ramos: ['', '', '', '', '', '', '', '', '1'], link: 'https://www.newe.com.br' },
-    { seguradora: 'PORTO SEGURO', ramos: ['1', '1', '1', '1', '1', '1', '1', '1', ''], link: 'https://www.portoseguro.com.br' },
-    { seguradora: 'POTTENCIAL', ramos: ['', '', '', '1', '1', '', '', '', '1'], link: 'https://www.pottencial.com.br' },
-    { seguradora: 'PRUDENTIAL', ramos: ['', '', '', '', '', '1', '', '', ''], link: 'https://www.prudential.com.br' },
-    { seguradora: 'SANCOR', ramos: ['1', '', '', '1', '', '', '', '', ''], link: 'https://www.sancor.com.br' },
-    { seguradora: 'SOMBRERO', ramos: ['', '', '', '', '', '', '','', '1'], link: 'https://www.sombrero.com.br' },
-    { seguradora: 'SUHAI', ramos: ['1', '1', '1', '', '', '', '', '', ''], link: 'https://www.suhai.com.br' },
-    { seguradora: 'SULAMERICA', ramos: ['', '', '', '', '', '1', '', '', ''], link: 'https://www.sulamerica.com.br' },
-    { seguradora: 'SURA', ramos: ['', '', '', '', '1', '', '', '', ''], link: 'https://www.sura.com.br' },
-    { seguradora: 'SWISSRE', ramos: ['', '', '', '', '', '', '', '1', ''], link: 'https://www.swissre.com.br' },
-    { seguradora: 'TOKIO MARINE', ramos: ['1', '1', '1', '1', '1', '1', '1', '', ''], link: 'https://www.tokiomarine.com.br' },
-    { seguradora: 'ZURICH', ramos: ['1', '1', '1', '1', '1', '1', '', '', ''], link: 'https://www.zurich.com.br' }
+//                                         🚘   🚛   🏍   🏭   🏠   ❤   🏢   🚜   💼	
+    { seguradora: 'ALLIANZ     ', ramos: ['1', '1', '1', '1', '1', '1', '1', '1', '1'], ativo: '1', link: 'https://www.allianz.com.br'     },
+    { seguradora: 'HDI         ', ramos: ['1', '1', '1', '1', '1', ' ', '1', ' ', ' '], ativo: '1', link: 'https://www.hdi.com.br/hdidigital/'         },
+    { seguradora: 'LIBERTY     ', ramos: ['1', '1', ' ', '1', '1', '1', ' ', ' ', ' '], ativo: '1', link: 'https://www.liberty.com.br'     },
+    { seguradora: 'MAPFRE      ', ramos: ['1', '1', '1', '1', '1', '1', ' ', '1', ' '], ativo: '1', link: 'https://www.mapfre.com.br'      },
+    { seguradora: 'POTTENCIAL  ', ramos: [' ', ' ', ' ', '1', '1', ' ', ' ', ' ', '1'], ativo: '1', link: 'https://www.pottencial.com.br'  },
+    { seguradora: 'SULAMERICA  ', ramos: [' ', ' ', ' ', ' ', ' ', '1', ' ', ' ', ' '], ativo: '1', link: 'https://www.sulamerica.com.br'  },
+    { seguradora: 'SUHAI       ', ramos: ['1', '1', '1', ' ', ' ', ' ', ' ', ' ', ' '], ativo: '1', link: 'https://www.suhai.com.br'       },
+    { seguradora: 'TOKIO MARINE', ramos: ['1', '1', '1', '1', '1', '1', '1', ' ', ' '], ativo: '1', link: 'https://www.tokiomarine.com.br' },
+    { seguradora: 'AIG         ', ramos: [' ', '1', ' ', '1', ' ', ' ', ' ', '1', '1'], ativo: '0', link: '.' },
+    { seguradora: 'AKAD        ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'ARGO        ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'AXA         ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'BERKLEY     ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'BRADESCO    ', ramos: ['1', '1', '1', '1', '1', '1', '1', ' ', ' '], ativo: '0', link: '.' },
+    { seguradora: 'CHUBB       ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'ESSOR       ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'EXCELSIOR   ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'EZZE        ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'FAIRFAX     ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'FATOR       ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'KOVR        ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'MAG         ', ramos: [' ', ' ', ' ', ' ', ' ', '1', ' ', ' ', ' '], ativo: '0', link: '.' },
+    { seguradora: 'METLIFE     ', ramos: [' ', ' ', ' ', ' ', ' ', '1', ' ', ' ', ' '], ativo: '0', link: '.' },
+    { seguradora: 'NEWE        ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'PORTO SEGURO', ramos: ['1', '1', '1', '1', '1', '1', '1', '1', ' '], ativo: '0', link: '.' },
+    { seguradora: 'PRUDENTIAL  ', ramos: [' ', ' ', ' ', ' ', ' ', '1', ' ', ' ', ' '], ativo: '0', link: '.' },
+    { seguradora: 'SANCOR      ', ramos: ['1', ' ', ' ', '1', ' ', ' ', ' ', ' ', ' '], ativo: '0', link: '.' },
+    { seguradora: 'SOMBRERO    ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1'], ativo: '0', link: '.' },
+    { seguradora: 'SURA        ', ramos: [' ', ' ', ' ', ' ', '1', ' ', ' ', ' ', ' '], ativo: '0', link: '.' },
+    { seguradora: 'SWISSRE     ', ramos: [' ', ' ', ' ', ' ', ' ', ' ', ' ', '1', ' '], ativo: '0', link: '.' },
+    { seguradora: 'ZURICH      ', ramos: ['1', '1', '1', '1', '1', '1', ' ', ' ', ' '], ativo: '0', link: '.' }
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
     const menuHorizontal = document.getElementById('menu-horizontal');
     const menuVertical = document.getElementById('menu-vertical');
+	let activeButton = null; // Definindo a variável aqui
 
     ramosSeguros.forEach((ramo, index) => {
         const btn = document.createElement('button');
         btn.textContent = ramo;
         btn.addEventListener('click', () => {
             displaySeguradoras(index);
+		    if (activeButton) {
+                activeButton.classList.remove('active');
+            }
+            btn.classList.add('active');
+            activeButton = btn;	
+			
         });
         menuHorizontal.appendChild(btn);
     });
@@ -60,7 +68,8 @@ document.addEventListener('DOMContentLoaded', () => {
     function displaySeguradoras(index) {
         menuVertical.innerHTML = '';
         seguradoras.forEach(entry => {
-            if (entry.ramos[index] === '1') {
+          //if (entry.ramos[index] === '1') {
+			if (entry.ramos[index] === '1' && entry.ativo === '1') {	
                 const link = document.createElement('a');
                 link.href = entry.link;
                 link.target = '_blank'; // Abre o link em uma nova aba
@@ -75,4 +84,3 @@ document.addEventListener('DOMContentLoaded', () => {
         menuVertical.classList.remove('hidden');
     }
 });
-
