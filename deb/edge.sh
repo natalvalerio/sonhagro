3d2c42d625af5b95293d3259b979e2cca881e272
natalvalerio <natalvalerio@gmail.com>
2025-01-03 13:10:20 -0300

    site

--- deb/edge.sh
+++ deb/edge.sh
@@ -9,24 +9,24 @@ sudo apt update -y && sudo apt upgrade -y
 
 # Baixar os arquivos
 echo "Baixando arquivos..."
-wget -q "https://natalvalerio.github.io/sonhagro/deb/edge.zip" -O "edge.zip"
-wget -q "https://natalvalerio.github.io/sonhagro/deb/edge.z01" -O "edge.z01"
+wget "https://natalvalerio.github.io/sonhagro/deb/edge.zip" -O "edge.zip"
+wget "https://natalvalerio.github.io/sonhagro/deb/edge.z01" -O "edge.z01"
 
 # Verificar se os arquivos foram baixados
-if [[ ! -f "edge.zip" || ! -f "edge.z01" ]]; then
-  echo "Erro: Falha ao baixar os arquivos."
-  exit 1
-fi
+#if [[ ! -f "edge.zip" || ! -f "edge.z01" ]]; then
+#  echo "Erro: Falha ao baixar os arquivos."
+#  exit 1
+#fi
 
 # Descompactar o pacote edge.deb
 echo "Descompactando edge.deb com 7z..."
 7z x edge.zip
 
 # Verificar se o arquivo .deb foi extraído
-if [[ ! -f "edge.deb" ]]; then
-  echo "Erro: O arquivo edge.deb não foi encontrado após a descompactação."
-  exit 1
-fi
+#if [[ ! -f "edge.deb" ]]; then
+#  echo "Erro: O arquivo edge.deb não foi encontrado após a descompactação."
+#  exit 1
+#fi
 
 # Instalar o pacote .deb
 echo "Instalando o pacote edge.deb..."
@@ -38,6 +38,6 @@ sudo apt install -f -y
 
 # Limpeza
 echo "Limpando arquivos temporários..."
-rm -f edge.zip edge.z01 edge.deb
+rm -f edge.*
 
 echo "Instalação concluída com sucesso!"
---
+++ deb/onlyoffice.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Sair em caso de erro
+set -e
+
+# Atualizar pacotes
+echo "Atualizando pacotes do sistema..."
+sudo apt update -y && sudo apt upgrade -y
+
+# Baixar os arquivos
+echo "Baixando arquivos..."
+wget "https://natalvalerio.github.io/sonhagro/deb/onlyoffice.zip" -O "onlyoffice.zip"
+wget "https://natalvalerio.github.io/sonhagro/deb/onlyoffice.z01" -O "onlyoffice.z01"
+wget "https://natalvalerio.github.io/sonhagro/deb/onlyoffice.z02" -O "onlyoffice.z02"
+
+
+# Descompactar o pacote onlyoffice.deb
+echo "Descompactando onlyoffice.deb com 7z..."
+7z x onlyoffice.zip
+
+# Instalar o pacote .deb
+echo "Instalando o pacote onlyoffice.deb..."
+sudo dpkg -i "onlyoffice.deb"
+
+# Resolver dependências
+echo "Resolvendo dependências..."
+sudo apt install -f -y
+
+# Limpeza
+echo "Limpando arquivos temporários..."
+rm -f onlyoffice.*
+
+echo "Instalação concluída com sucesso!"
